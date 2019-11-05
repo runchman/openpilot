1b2ab410a8fa9a81a1fa1429fa3862d4754caf61
john <j_runchey@oakbrookinvest.com>
2019-11-05 12:34:18 -0600

    change some gas and brake tuning params

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -261,10 +261,14 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 10.25
       tire_stiffness_factor = 0.75 # not optimized yet
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.20], [0.04]]
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+      # BP values are speeds - 0 m/s, 10 m/s, 35 m/s
+      # which is 0 MPH, 22 MPH, and 78 MPH
+      ret.longitudinalTuning.kpBP = [0., 10., 35.]
+      # These are corresponding Kp values with the above speeds
+      ret.longitudinalTuning.kpV = [0.6, 0.4, 0.3]
       ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kiV = [0.08, 0.02]
+      # J.R. no Kf ???? defaults to 1.0 what the heck
 
     elif candidate == CAR.ACURA_RDX:
       stop_and_go = False
@@ -384,8 +388,8 @@ class CarInterface(CarInterfaceBase):
     
     #ret.gasMaxBP = [0.]  # m/s
     #ret.gasMaxV = [0.6] if ret.enableGasInterceptor else [0.] # max gas allowed
-    ret.brakeMaxBP = [5., 20.]  # m/s
-    ret.brakeMaxV = [1., 0.8]   # max brake allowed
+    ret.brakeMaxBP = [0., 40.]  # m/s
+    ret.brakeMaxV = [1.0, 0.8]   # max brake allowed
 
     ret.longitudinalTuning.deadzoneBP = [0.]
     ret.longitudinalTuning.deadzoneV = [0.]
